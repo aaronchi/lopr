5ae09f1cfa0593c26f93a309131385d81f419359
Aaron <aaron@Iris.local>
2012-08-18 20:30:38 -0700

    order

--- db/migrate/20120819032453_add_position_to_products.rb
+++ db/migrate/20120819032453_add_position_to_products.rb
@@ -2,7 +2,7 @@ class AddPositionToProducts < ActiveRecord::Migration
   def change
     add_column :products, :position, :integer
     Product.reset_column_information
-    Product.all.each_with_index do |p,i|
+    Product.order('id').each_with_index do |p,i|
       p.update_column :position, i+1
     end
   end
